066004a4f1bd4bce9159fcda956ef6cf87e15927
KabokAkos <99403467+KabokAkos@users.noreply.github.com>
2024-10-04 16:00:38 +0200

    Update string2.js

--- string_method/string2.js
+++ string_method/string2.js
@@ -1,38 +1,30 @@
 let txt = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde quo autem voluptatibus repellendus, ut magnam sunt? Quae, explicabo repudiandae enim nesciunt esse facere magni voluptas, odit officia vitae repellat molestias.";
-console.log("1. feladat: ")
+console.log("Exercise 1.");
 console.log(txt.slice(0, 6));
-console.log("2. feladat: ")
+
+console.log("Exercise 2.");
 console.log(txt.slice(4, 9));
-console.log("3. feladat: ")
+
+console.log("Exercise 3.");
 console.log(txt.substring(4, txt.lenght));
-console.log("4. feladat: ")
+
+console.log("Exercise 4.");
 console.log(txt.substring((4, 10)));
-console.log("5. feladat: ")
+
+console.log("Exercise 5.");
 console.log(txt.toUpperCase(txt));
 
-
-
-let modifiedText = '';
-let ePositions = [];
-
+console.log("Exercise 6.");
+let modifiedforsix = '';
 for (let i = 0; i < txt.length; i++) {
-    let char = txt[i];
-
-    if (i % 2 === 1) {
-        char = char.toUpperCase();
-    }
-    modifiedText += char;
+    result6 += i % 2 === 0 ? txt[i].toLowerCase() : txt[i].toUpperCase();
 }
-console.log("6. feladat: ")
-console.log(modifiedText);
+console.log(modifiedforsix);
 
-for (let i = 0; i < txt.length; i++) {
-    let char = txt[i];
-    
-    if (char === 'e') {
-        char += 'E';
-    }
-}
+console.log("Exercise 7.");
+let modifiedforseven = txt.replace(/e/g, "E");
+console.log(modifiedforseven);
 
-console.log("7. feladat:");
-console.log(ePositions);
+console.log("Exercise 8.");
+let modifiedforeight = txt.split("e");
+console.log(modifiedforeight);
